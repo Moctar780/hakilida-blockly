756c94b2ec1a41610a64fc3d83a504ee46f4d9c0
Moctar780 <moctar740@gmail.com>
2026-07-03 21:56:29 +0000

    Fix Android build: re-init Android project after config change

---
+++ src-tauri/gen/android/app/src/main/java/com/starter_kit_racing/game/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.starter_kit_racing.game
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
